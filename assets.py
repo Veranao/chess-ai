41b8883cd698907ef518b41ebbe522dd440e7971
Veranao <victor.cao145@gmail.com>
2025-06-27 01:45:23 -0400

    Impemented turn selector and pseudo valid move checker

--- assets.py
+++ assets.py
@@ -11,7 +11,7 @@ def load_pieces(side, image_arr, image_arr_small, piece_list):
         small_piece = pygame.transform.scale(piece, (20, 20))
         image_arr_small.append(small_piece)
 
-def draw_board(screen, width, height, turn_step, big_font):
+def draw_board(screen, width, height, turn_step, big_font, turn_prompt):
     for i in range(32):
         row = i // 4
         column = i % 4
@@ -26,16 +26,27 @@ def draw_board(screen, width, height, turn_step, big_font):
     pygame.draw.rect(screen, (0, 53, 0), [0, 800, width, 100], 5)
     pygame.draw.rect(screen, (0, 53, 0), [800, 0, 200, height], 5)
 
-    turn_prompt = ['White: Select a piece to move!', 'White: Select where to go!', 'Black: Select a piece to move!', 'Black: Select where to go!']
     screen.blit(big_font.render(turn_prompt[turn_step], True, 'black'), (20, 820))
 
     for i in range(9):
         pygame.draw.line(screen, 'black', (0, 100 * i), (800, 100 * i), 5)
         pygame.draw.line(screen, 'black', (100 * i, 0), (100 * i, 800), 5)
 
-def draw_pieces(piece_list, white_pieces, black_pieces, white_images, black_images, white_locations, black_locations, screen):
+def draw_pieces(piece_list, white_pieces, black_pieces, white_images, black_images, white_locations, black_locations, screen, turn_step, selection):
     for i in range(len(white_pieces)):
         index = piece_list.index(white_pieces[i])
-
         screen.blit(white_images[index], (white_locations[i][0] * 100 + 7, white_locations[i][1] * 100 + 5))
+
+        if turn_step < 2:
+            if selection == i:
+                pygame.draw.rect(screen, 'red', [white_locations[i][0] * 100 + 1, white_locations[i][1] * 100 + 1, 100, 100], 3)
+
+
+    for i in range(len(black_pieces)):
+        index = piece_list.index(black_pieces[i])
         screen.blit(black_images[index], (black_locations[i][0] * 100 + 7, black_locations[i][1] * 100 + 5))
+
+        if turn_step > 1:
+            if selection == i:
+                pygame.draw.rect(screen, 'red', [black_locations[i][0] * 100 + 1, black_locations[i][1] * 100 + 1, 100, 100], 3)
+    
--- main.py
+++ main.py
@@ -14,6 +14,7 @@ timer = pygame.time.Clock()
 frames_per_second = 60
 pygame.display.set_caption('Chess in Python')
 
+
 #chess pieces, locations, and piece capturing arrays
 white_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn' ]
 black_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn' ]
@@ -44,18 +45,65 @@ small_black_images = []
 load_pieces("b", black_images, small_black_images, piece_list)
 load_pieces("w", white_images, small_white_images, piece_list)
 
+turn_prompt = ['White: Select a piece to move!', 'White: Select where to go!', 'Black: Select a piece to move!', 'Black: Select where to go!']
+
+def check_options(pieces, locations, turn):
+    pass
+    #Implement to check the move list of all the pieces in the game and their valid moves at a given state of the game (depending on what turn with one turn of look-ahead)
+
+black_options = check_options(black_pieces, black_locations, 'black')
+white_options = check_options(white_pieces, white_locations, 'white')
 run = True
+
 while run:
     timer.tick(frames_per_second)
     screen.fill((153, 80, 0))
-    draw_board(screen, WIDTH, HEIGHT, turn_step, big_font)
-    draw_pieces(piece_list, white_pieces, black_pieces, white_images, black_images, white_locations, black_locations, screen)
+    draw_board(screen, WIDTH, HEIGHT, turn_step, big_font, turn_prompt)
+    draw_pieces(piece_list, white_pieces, black_pieces, white_images, black_images, white_locations, black_locations, screen, turn_step, selection)
 
     #event handling for quitting the game
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            chess_coordinate = (event.pos[0] // 100, event.pos[1] // 100)
+
+            if turn_step < 2:
+                if chess_coordinate in white_locations:
+                    selection = white_locations.index(chess_coordinate)
+                    if turn_step == 0:
+                        turn_step = 1
+                if chess_coordinate in valid_moves and selection != 100:
+                    white_locations[selection] = chess_coordinate
+                    if chess_coordinate in black_locations:
+                        black_piece = black_locations.index(chess_coordinate)
+                        captured_white_pieces.append(black_pieces[black_piece])
+                        black_pieces.pop(black_piece)
+                        black_locations.pop(black_piece)
+                    black_options = check_options(black_pieces, black_locations, 'black')
+                    white_options = check_options(white_pieces, white_locations, 'white')
+                    turn_step = 2
+                    selection = 100
+                    valid_moves = []
+
+            if turn_step > 1:
+                if chess_coordinate in black_locations:
+                    selection = black_locations.index(chess_coordinate)
+                    if turn_step == 2:
+                        turn_step = 3
+                if chess_coordinate in valid_moves and selection != 100:
+                    black_locations[selection] = chess_coordinate
+                    if chess_coordinate in white_locations:
+                        white_piece = white_locations.index(chess_coordinate)
+                        captured_black_pieces.append(white_pieces[white_piece])
+                        white_pieces.pop(white_piece)
+                        white_locations.pop(white_piece)
+                    black_options = check_options(black_pieces, black_locations, 'black')
+                    white_options = check_options(white_pieces, white_locations, 'white')
+                    turn_step = 0
+                    selection = 100
+                    valid_moves = []
     
     pygame.display.flip()
 pygame.quit()
